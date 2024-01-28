fbced5e712bc20bab623d59829bf1a115bd15f46
zeinfnoureddin <zein.ne.30@gmail.com>
2024-01-28 21:01:00 +0200

    initial commit

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,51 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      customers: ["Zero to Mastery", "NASA"],
+      flightNumber: latestFlightNumber,
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+function doesLaunchExist(launchId) {
+  return launches.has(launchId);
+}
+
+module.exports = {
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
+  doesLaunchExist,
+};
---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,61 @@
+// const { planets } = require("../../models/planets.model");
+
+// function getAllPlanets(req, res) {
+//   return res.status(200).json(planets);
+// }
+
+// module.exports = {
+//   getAllPlanets,
+// };
+
+const {
+  getAllLaunches,
+  addNewLaunch,
+  doesLaunchExist,
+  abortLaunchById,
+} = require("../../models/launches.model");
+
+function httpGetAllLaunches(req, res) {
+  return res.status(200).json(getAllLaunches());
+}
+
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.target ||
+    !launch.launchDate
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property",
+    });
+  }
+  launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date",
+    });
+  }
+
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+  if (!doesLaunchExist(launchId)) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+  aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+}
+
+module.exports = {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+  httpAbortLaunch,
+};
